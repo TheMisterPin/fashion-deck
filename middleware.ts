a37a0a980a040d6662169ff5714931d26755ad9e
TheMisterPin <pinmichele23@gmail.com>
2024-11-17 21:58:08 +0100

    Fixed eslint issue in IDE

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,24 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isProtectedRoute = createRouteMatcher(['/outfits(.*)', '/wardrobe(.*)'])
 
 export default clerkMiddleware((auth, req) => {
-   (req: any) => ({
-    secretKey : process.env.CLERK_SECRET_KEY
+  ;(req: any) => ({
+    secretKey: process.env.CLERK_SECRET_KEY
   })
   if (!auth().userId && isProtectedRoute(req)) {
     // Add custom logic to run before redirecting
 
     return auth().redirectToSignIn()
   }
- 
 })
+
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-};
+    '/(api|trpc)(.*)'
+  ]
+}
